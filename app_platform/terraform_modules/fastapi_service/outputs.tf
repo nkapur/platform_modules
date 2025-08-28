05287b8bd57e82f630624e635aa67c11b583c693
Nav <nav17kapur@gmail.com>
2025-08-27 23:02:27 -0700

    fixing unclosed block error and adding guardrails

--- app_platform/terraform_modules/fastapi_service/outputs.tf
+++ app_platform/terraform_modules/fastapi_service/outputs.tf
@@ -1,7 +1,7 @@
 output "ecr_repository_url" {
   description = "The URL of the created ECR repository."
   value       = aws_ecr_repository.app_repository.repository_url
-
+}
 
 output "ecr_repository_name" {
   description = "The name of the ECR repository."
